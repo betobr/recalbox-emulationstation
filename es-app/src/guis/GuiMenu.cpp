d28f1e88710d5c48df628b95545d1c5cb54fbd26
Unknown <roberto0205@gmail.com>
2017-03-19 02:25:15 -0300

    Over rpi3

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -248,7 +248,9 @@ GuiMenu::GuiMenu(Window *window) : GuiComponent(window), mMenu(window, _("MAIN M
                      overclock_choice->add(_("HIGH (1050Mhz)"), "rpi2-high", currentOverclock == "rpi2-high");
                      overclock_choice->add(_("NONE (900Mhz)"), "none", currentOverclock == "none");
 #elif RPI_VERSION == 3
-                     overclock_choice->add(_("NONE (1200Mhz)"), "none", true);
+		     std::string currentOverclock = Settings::getInstance()->getString("Overclock");
+		     overclock_choice->add(_("HIGH (1250Mhz)"), "rpi3-high", currentOverclock == "rpi3-high");
+                     overclock_choice->add(_("NONE (1200Mhz)"), "none-rpi3", currentOverclock == "none-rpi3");
 #endif
 #else
                      overclock_choice->add(_("NONE"), "none", true);
